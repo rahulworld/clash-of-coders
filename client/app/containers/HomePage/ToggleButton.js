6d2e7609d0901d292da933bce5c8aa46dbb8ceba
Abhishek <apple@Apples-MacBook-Pro.local>
2021-08-12 20:12:43 +0530

    client for hackathon dashboard

---
+++ client/app/containers/HomePage/ToggleButton.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const CheckedIcon = () => <>🌞</>;
+const UncheckedIcon = () => <>🌜</>;
+
+const ToggleButton = props => {
+  const [toggle, setToggle] = useState(false);
+  const { defaultChecked, onChange, disabled, className } = props;
+
+  useEffect(() => {
+    if (defaultChecked) {
+      setToggle(defaultChecked);
+    }
+  }, [defaultChecked]);
+
+  const triggerToggle = () => {
+    if (disabled) {
+      return;
+    }
+
+    setToggle(!toggle);
+
+    if (typeof onChange === 'function') {
+      onChange(!toggle);
+    }
+  };
+
+  const getIcon = type => {
+    const { icons } = props;
+    if (!icons) {
+      return null;
+    }
+
+    return icons[type] === undefined
+      ? ToggleButton.defaultProps.icons[type]
+      : icons[type];
+  };
+
+  const toggleClasses = classNames(
+    'wrg-toggle',
+    {
+      'wrg-toggle--checked': toggle,
+      'wrg-toggle--disabled': disabled,
+    },
+    className,
+  );
+
+  const onKeyPressHandler = () => null;
+
+  return (
+    <div
+      onClick={triggerToggle}
+      className={toggleClasses}
+      onKeyPress={onKeyPressHandler}
+      role="button"
+      tabIndex="0"
+    >
+      <div className="wrg-toggle-container">
+        <div className="wrg-toggle-check">
+          <span>{getIcon('checked')}</span>
+        </div>
+        <div className="wrg-toggle-uncheck">
+          <span>{getIcon('unchecked')}</span>
+        </div>
+      </div>
+      <div className="wrg-toggle-circle" />
+      <input
+        type="checkbox"
+        aria-label="Toggle Button"
+        className="wrg-toggle-input"
+      />
+    </div>
+  );
+};
+
+ToggleButton.defaultProps = {
+  icons: {
+    checked: <CheckedIcon />,
+    unchecked: <UncheckedIcon />,
+  },
+};
+
+ToggleButton.propTypes = {
+  disabled: PropTypes.bool,
+  defaultChecked: PropTypes.bool,
+  className: PropTypes.string,
+  onChange: PropTypes.func,
+  icons: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.shape({
+      checked: PropTypes.node,
+      unchecked: PropTypes.node,
+    }),
+  ]),
+};
+
+export default ToggleButton;
