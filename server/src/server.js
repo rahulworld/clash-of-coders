17318b52a56751ae4b73b572cc2f023edc336b28
Rahul Chauhan <chauhanr371@gmail.com>
2021-08-12 19:22:27 +0530

    added config

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const helmet = require('helmet');
+const morgan = require('morgan');
 const {connectMongoDB} = require('./services/mongo');
 const {writeDefultPages} = require('./models/dashboard.model');
 
@@ -20,6 +21,7 @@ app.use(cors({
     preflightContinue: false,
     optionsSuccessStatus: 204
 }));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
